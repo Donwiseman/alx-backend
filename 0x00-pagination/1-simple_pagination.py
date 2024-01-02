f35484f27430bd6762e5d2833dfa1f1583c59bf6
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-02 05:27:05 +0000

    1-simple_pagination.py pycode error correction

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,10 +35,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Returns the requested page as a list. """
-            assert isinstance(page, int) and page >= 0
-            assert isinstance(page_size, int) and page_size > 0
-            pg = index_range(page, page_size)
-            self.dataset()
-            return self.__dataset[pg[0]: pg[1]]
-    
+        """Returns the requested page as a list."""
+        assert isinstance(page, int) and page >= 0
+        assert isinstance(page_size, int) and page_size > 0
+        pg = index_range(page, page_size)
+        self.dataset()
+        return self.__dataset[pg[0]: pg[1]]
