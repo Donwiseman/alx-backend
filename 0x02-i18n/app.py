0e9b6e46842727b494c55ead1993707946a9fb4f
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-10 04:54:50 +0000

    Basic flask app

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This contains the declaration for the flask application."""
+
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Homepage of website. """
+    return render_template('0-index.html')
