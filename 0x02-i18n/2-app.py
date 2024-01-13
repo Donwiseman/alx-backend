71901c56d52c8c47f880feed2fc1546a22c2b39f
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-13 01:44:52 +0000

    Added a locale selector using request.accept

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""This contains the declaration for the flask application."""
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Set default configurations for the application."""
+    # Set the available languages
+    LANGUAGES = ['en', 'es', 'fr']
+
+    # Set the default language
+    BABEL_DEFAULT_LOCALE = 'en'
+
+    # set the default timezone
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Homepage of website. """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
